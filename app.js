d80a232b43f71245888613fe9fbba242b7ddcbb7
Mohammed Bettaoui <bettapro100@gmail.com>
2020-06-20 17:08:35 +0100

    Solving Problems

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
-const port = process.env.PORT || 2020;
+const port = process.env.PORT || 7777;
 
 
 
@@ -21,8 +21,9 @@ connection.connect((error)=>{
 });
 
 
-app.use(express.static(path.join(__dirname, 'public','css')));
-app.use(express.static(path.join(__dirname, 'public', 'img')));
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public','img')));
+app.use(express.static(path.join(__dirname,'public','css')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
@@ -38,7 +39,6 @@ app.get('/Home',(req, res) => {
 
     let sql = "SELECT * FROM produit";
     connection.query(sql, (err, rows) => {
-      console.log(rows);
         if(err) throw err;
         res.render('Produit', {
             title : 'All Products',
@@ -121,6 +121,8 @@ app.get('/addFournisseur',(req, res) => {
 
 });
 
+
+
 app.post('/SaveFournisseur',(req, res) => {
 
     let data = {id_fournisseur: req.body.id_f, nom: req.body.nom_f, tel: req.body.tel};
@@ -157,42 +159,142 @@ app.post('/SaveRayons',(req, res) => {
 
 
 
-app.get('/edit/:userId',(req, res) => {
+app.get('/editProduct/:id',(req, res) => {
 
-    const auteurId = req.params.userId;
-    let sql = `Select * from produit where id_produit = ${auteurId}`;
-    let query = connection.query(sql,(err, result) => {
+    const productId = req.params.id;
+
+    let sqlid =  `SELECT * FROM produit WHERE id_produit = ${productId}`;
+     connection.query(sqlid,(err, result) => {
+        let row;
         if(err) throw err;
-        res.render('UpdateAuteur', {
-            title : 'Modification produit',
-            row : result[0]
-        });
-    });
+          row = result[0];
+          let idR;
+          let idF;
+          let sql1 = "SELECT id_rayon FROM rayons";
+          let sql2 =  "SELECT id_fournisseur FROM fournisseur"
+          connection.query(sql1, (err, rows1) => {
+
+            idR = rows1;
+              if(err) throw err;
+              connection.query(sql2, (err, rows2) => {
+                idF = rows2;
+                  if(err) throw err;
+          res.render('UpdateProduit', {
+              title : 'Modification produit',
+              row : row,
+              idR : idR,
+              idF : idF
+          });
+      });
+  });
+
+
+
+});
 });
 
-app.post('/update',(req, res) => {
-
-  let userId = req.body.id;
 
 
-    let sql = `update auteurs SET Nom = '${req.body.nom}', Age = ${req.body.age}, Nationalite = '${req.body.natio}' where id_Auteur = ${userId}`;
-    let query = connection.query(sql,(err, results) => {
+
+app.post('/updateProduct',(req, res) => {
+
+  let productId = req.body.idP;
+console.log(productId);
+
+    let sql = `UPDATE produit SET id_rayon = '${req.body.idrayon}', id_fournisseur = ${req.body.idfourn}, nom_produit = '${req.body.nom}', quantite = ${req.body.Qun} where id_produit = ${productId}`;
+     connection.query(sql,(err, results) => {
+
       if(err) throw err;
-      res.redirect('/');
+      res.redirect('/Home');
     });
 });
 
 
 
-app.get('/delete/:userId',(req, res) => {
-    const userId = req.params.userId;
-    let sql = `DELETE from auteurs where id_Auteur = ${userId}`;
+app.get('/editForn/:FournId',(req, res) => {
+
+    let FournId = req.params.FournId;
+    let sql = `Select * from fournisseur where id_fournisseur = ${FournId}`;
+     connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('updateForn', {
+            title : 'Modification Fournisseur',
+            row : result[0]
+
+        });
+
+    });
+});
+
+app.post('/updateForn',(req, res) => {
+
+  let fornId = req.body.idF;
+
+
+    let sql = `UPDATE fournisseur SET nom = '${req.body.nomf}', tel = ${req.body.tel} where id_fournisseur = ${fornId}`;
+    connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/Fournisseur');
+    });
+});
+
+
+
+app.get('/editRayon/:RayonId',(req, res) => {
+
+    let RayonId = req.params.RayonId;
+    let sql = `Select * from rayons where id_rayon = ${RayonId}`;
+     connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('updateRayon', {
+            title : 'Modification Rayons',
+            row : result[0]
+
+        });
+
+    });
+});
+
+app.post('/updateRayon',(req, res) => {
+
+  let RayonId = req.body.idR;
+
+
+    let sql = `UPDATE rayons SET nbr_produit = '${req.body.nbrP}', nbr_fornisseur = ${req.body.nbrF} where id_rayon = ${RayonId}`;
+    connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/rayons');
+    });
+});
+
+
+
+app.get('/deleteProduct/:productId',(req, res) => {
+    const productId = req.params.productId;
+    let sql = `DELETE from produit where id_produit = ${productId}`;
     let query = connection.query(sql,(err, result) => {
         if(err) throw err;
-        res.redirect('/');
+        res.redirect('/Home');
     });
 });
 
+app.get('/deleteForn/:fornId',(req, res) => {
+    const fornId = req.params.fornId;
+    let sql = `DELETE from fournisseur where id_fournisseur = ${fornId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/Fournisseur');
+    });
+});
+
+app.get('/deleteRayon/:rayonId',(req, res) => {
+    const rayonId = req.params.rayonId;
+    let sql = `DELETE from rayons where id_rayon = ${rayonId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/rayons');
+    });
+});
 
 app.listen(port, (error)=>{
   console.log(`Listening on port ${port}`);
